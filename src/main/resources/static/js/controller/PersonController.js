4eb7cadd7a3d897e3d9dfbc91335a0a3ae06c950
robsoncassol <robsoncassol@gmail.com>
2015-10-22 16:56:03 -0200

    person form

--- src/main/resources/static/js/controller/PersonController.js
+++ src/main/resources/static/js/controller/PersonController.js
@@ -1,17 +1,24 @@
 app.controller('PersonController', function($scope, PersonService) {
 
-	$scope.people = []
+	$scope.people = [];
+	$scope.person = {};
 	
 	function listPersons(){
 		PersonService.query(function(retorno) {
 	        $scope.people = retorno;
 	    });
-	}
+	};
 	
 	listPersons();
 	
 	$scope.remove = function(person) {
 		PersonService.delete({id: person.id}, function(status) {
+			listPersons();
+        });
+    };
+    
+    $scope.save = function() {
+		PersonService.save($scope.person, function(status) {
 			listPersons();
         });
     };
